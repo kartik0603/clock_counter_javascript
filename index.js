cb9752049ff18ed69524bbeb2dc356036bfc1f28
kartik0603 <kartikhirapara800@gmail.com>
2024-05-19 13:18:18 +0530

    clock

--- index.js
+++ index.js
@@ -1,50 +1,35 @@
-
 setInterval(showTime, 1000);
 function showTime() {
     let time = new Date();
-    let hour =
-        time.getHours();
-    let min =
-        time.getMinutes();
-    let sec =
-        time.getSeconds();
-    am_pm = "AM";
-    let day=time.getFullYear();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    let am_pm = "AM";
 
     if (hour >= 12) {
-        if (hour > 12)
-            hour -= 12;
+        if (hour > 12) hour -= 12;
         am_pm = "PM";
     } else if (hour == 0) {
-        hr = 12;
+        hour = 12;
         am_pm = "AM";
     }
 
-    hour =
-        hour < 10
-            ? "0" + hour
-            : hour;
-    min =
-        min < 10
-            ? "0" + min
-            : min;
-    sec =
-        sec < 10
-            ? "0" + sec
-            : sec;
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
 
-    let currentTime =
-        hour +
-        ":" +
-        min +
-        ":" +
-        sec +
-        am_pm;
+    let currentTime = hour + ":" + min + ":" + sec + " " + am_pm;
 
-    document.getElementById(
-        "clock"
-    ).innerHTML =
-        currentTime;
+    let day = time.getDate();
+    let month = time.getMonth() + 1;
+    let year = time.getFullYear();
+
+    day = day < 10 ? "0" + day : day;
+    month = month < 10 ? "0" + month : month;
+
+    let currentDate = day + "/" + month + "/" + year;
+
+    document.getElementById("clock").innerHTML = currentTime + "<br>" + currentDate;
 }
 
 showTime();
